aea71cea9c29fb47b3915ef137c7c682b4739486
emiliars20 <emiliarsierra@gmail.com>
2024-10-23 01:27:39 -0300

    cambios peso y altura

--- pokemon.js
+++ pokemon.js
@@ -20,31 +20,35 @@ function searchPokemon() {
   
 // Función para mostrar los detalles del Pokémon
 function displayPokemonDetails(pokemon) {
-    const name = pokemon.name;
-    const id = String(pokemon.id).padStart(4, '0'); // Formatea el ID a 4 cifras
-    const abilities = pokemon.abilities.map(ability => ability.ability.name).join(', ');
-    const height = pokemon.height;
-    const weight = pokemon.weight;
-    const types = pokemon.types.map(type => type.type.name).join(', ');
-    const imageUrl = pokemon.sprites.other['official-artwork'].front_default; // URL de la imagen
+  const name = pokemon.name;
+  const id = String(pokemon.id).padStart(4, '0'); // Formatea el ID a 4 cifras
+  const abilities = pokemon.abilities.map(ability => ability.ability.name).join(', ');
   
-    const details = `
-      <div class="card">
-        <img src="${imageUrl}" alt="${name}">
-        <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2> 
-        <p><strong>#</strong> ${id}</p>
-        <p><strong>Habilidades:</strong> ${abilities}</p>
-        <p><strong>Altura:</strong> ${height}m</p>
-        <p><strong>Peso:</strong> ${weight}kg</p>
-        <p><strong>Tipos:</strong> ${types}</p>
-      </div>
-    `;
-  
-    document.getElementById('pokemonDetails').innerHTML = details;
+  // Formateo de altura y peso
+  const height = (pokemon.height / 10).toFixed(2); // Convertir a metros y limitar a dos cifras
+  const weight = (pokemon.weight / 10).toFixed(1).replace('.', ','); // Convertir a decenas y reemplazar el punto por una coma
 
-    // Reproduce el sonido
-    const sound = document.getElementById('pokemonSound');
-    sound.currentTime = 0; // Reinicia el sonido para que siempre suene desde el inicio
-    sound.play();
-  }
+  const types = pokemon.types.map(type => type.type.name).join(', ');
+  const imageUrl = pokemon.sprites.other['official-artwork'].front_default; // URL de la imagen
+
+  const details = `
+    <div class="card">
+      <img src="${imageUrl}" alt="${name}">
+      <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+      <p><strong>#</strong> ${id}</p>
+      <p><strong>Habilidades:</strong> ${abilities}</p>
+      <p><strong>Altura:</strong> ${height} m</p>
+      <p><strong>Peso:</strong> ${weight} kg</p>
+      <p><strong>Tipos:</strong> ${types}</p>
+    </div>
+  `;
+
+  document.getElementById('pokemonDetails').innerHTML = details;
+
+  // Reproduce el sonido
+  const sound = document.getElementById('pokemonSound');
+  sound.currentTime = 0; // Reinicia el sonido para que siempre suene desde el inicio
+  sound.play();
+}
+
   
